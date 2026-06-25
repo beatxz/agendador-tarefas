be88809514f9de7ecb1da1fac9b005e6a977c3cc
bea <164930551+beatxz@users.noreply.github.com>
2026-06-25 16:01:55 -0300

    Adicionados metodos Get por email e por periodo

--- src/main/java/com/dev/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/dev/agendadortarefas/controller/TarefasController.java
@@ -3,8 +3,12 @@ package com.dev.agendadortarefas.controller;
 import com.dev.agendadortarefas.business.TarefasService;
 import com.dev.agendadortarefas.business.dto.TarefasDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -17,6 +21,17 @@ public class TarefasController {
     public ResponseEntity<TarefasDTO>gravarTarefas(@RequestBody TarefasDTO dto,
                                                    @RequestHeader("Authorization")String token){
         return ResponseEntity.ok(tarefasService.gravarTarefa(token, dto));
+    }
+    @GetMapping("/eventos")
+    public ResponseEntity<List<TarefasDTO>> buscaListaDeTarefasPorPeriodo(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataFinal){
+        return ResponseEntity.ok(tarefasService.buscaTerefasAgendadasPorPeriodo(dataInicial,dataFinal));
+    }
+    @GetMapping
+    public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail(@RequestHeader ("Authorization")String token){
+
+        return ResponseEntity.ok(tarefasService.buscarTarefasPorEmail(token));
     }
 
 }
